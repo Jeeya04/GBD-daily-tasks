bc0158daabb3f2afee785b8d76c819c7df1f21a1
Jeeya04 <161134495+Jeeya04@users.noreply.github.com>
2026-01-21 15:35:10 +0530

    Create pg2.py

---
+++ python_conditions/pg2.py
@@ -0,0 +1,10 @@
+# pg2: Evaluate student result using marks
+
+marks = int(input("Enter marks: "))
+
+if marks >= 80:
+    print("Distinction")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
