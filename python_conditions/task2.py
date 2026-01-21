0e869ccfa07b5c4ec141c0e23937b4c4f678849f
Jeeya04 <161134495+Jeeya04@users.noreply.github.com>
2026-01-21 15:48:01 +0530

    Create task2.py

---
+++ python_conditions/task2.py
@@ -0,0 +1,10 @@
+# Task 2: Student result based on marks
+
+score = int(input("Enter marks: "))
+
+if score >= 80:
+    print("Result: Distinction")
+elif score >= 40:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
